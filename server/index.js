8481ecc5dbec34658d125a317f91f76b493a686a
Camila Rodriguez <camirodridevelop@gmail.com>
2025-03-12 12:02:02 -0300

    instalacion express..

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 //permite la conexion a nuestra bd en mongodb
 const mongoose = require ("mongoose");
-
+const app = require("./app");
 //config dotenv
 require("dotenv").config();
 
@@ -8,16 +8,21 @@ require("dotenv").config();
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
+const ipServer = process.env.IP_SERVER;
+const apiVersion = process.env.API_VERSION;
 
-console.log(dbUser);
-console.log(dbPass);
-console.log(dbHost);
+const port = 3977;
 
 //hacemos la conexion a nuestra base de datos...
 const connectDB = async () => {
     try { 
         await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/`);
-        console.log("La conexion con la base de datos ha sido exitosa");
+        app.listen(port,() => {
+            console.log("=======================================");
+            console.log("================API REST===============");
+            console.log("=======================================");
+            console.log(`http://${ipServer}:${port}/api/${apiVersion}/`);
+        });
     }  catch (error) {
         console.log("Error al conectar a la base de datos." ,error);
     }
