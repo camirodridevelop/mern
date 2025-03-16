096f4f700d3c225bbf577df2c548b4984cc47b38
Camila Rodriguez <camirodridevelop@gmail.com>
2025-03-16 14:37:38 -0300

    rutas y registrar usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,46 @@
-function register (req, res){
-    console.log ("se ha ejecutado correctamente");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-    res.status(200).send({msg:"Todo Okey" });
+async function register (req, res){
+
+    //la logica de obtener los datos desde el front,aplicar alguna validacion,guardar en la base de datos
+    //recibir los datos
+
+    const {firstname, lastname, password, email }=req.body;
+
+    //validar dos datos
+
+    if(!email)res.status(400).send({ msg: 'El email es obligatorio'});
+    if(!password) res.status(400).send({ msg: 'La contaseña es obligatorio'});
+
+    //crear usuario
+    const user = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email.toLowerCase(),
+        password: password,
+        role:"user",
+        active: false,
+    
+    })
+
+    console.log(user)
+    //tambien se puede hacer con const User = new User(...req.body)
+    
+   
+    //antes de guardar en la base d edatos encriptar contraseña (bcryptjscls)
+    const salt= bcrypt.genSaltSync(10);
+    const hashPassword=bcrypt.hashSync(password, salt)
+
+    user.password= hashPassword;
+
+    //guardar el usuario en la base de datos
+    try{
+        await user.save();
+        res.status(200).send({ msg:"Usuario Guardado"});
+    }catch(error){
+        res.status(400).send({ msg:"Error al crear el ususario: " + error});
+    }
 }
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,16 +1,15 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema ({
     firstname: String,
     lastname: String,
     email:{
         type: String,
-        unique: true,
-    };
+        unique: true
+    },
     password: String,
     role:String,
     active:Boolean,
-    avatar: String,
 });
 
 module.exports= mongoose.model('User', UserSchema);
