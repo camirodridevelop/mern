4a9bb1160a91bdf9e95063d580a93f488d42996b
Cami <camirodridevelop@gmail.com>
2026-09-20 22:53:46 -0300

    Agrega rutas de login y de usuario actual (/user/me)

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const AuthController = require('../controllers/auth')
+const express = require("express");
+const AuthController = require("../controllers/auth");
 
 const api = express.Router();
 
-//rutas
-api.post('/auth/register', AuthController.register);
+api.post("/auth/register", AuthController.register);
+api.post("/auth/login", AuthController.login);
 
 module.exports = api;
